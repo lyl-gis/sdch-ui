f40b28545f5df91518919d5f8d4d33817df4a729
LYL <yanlong_lee@126.com>
2019-02-25 20:58:58 +0800

    2.25修改：1.数据入库界面的进度条下面添加数据总量，当前入库条数和当前耗时三个指标；2.修改索引管理（indexManage）界面，使操作更简洁:a.一进入该页面,表格中的数据即存在；b.改变删除和编辑索引的方式，右键表格中的某条记录即可选择删除；修改表格中的数据后点击保存，即可完成编辑

---
+++ src/main/java/edu/zju/gis/sdch/ui/MainPort.java
@@ -0,0 +1,59 @@
+package edu.zju.gis.sdch.ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainPort implements Initializable {
+    @FXML
+    private MenuBar mbSelectPage;
+
+    @FXML
+    private ImageView IVimage;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Menu toolMenu = new Menu("工具");
+        Menu manageMenu = new Menu("管理");
+        mbSelectPage.getMenus().addAll(toolMenu, manageMenu);
+        MenuItem toolMenuImport = new MenuItem("入库工具");
+        MenuItem manageMenuIndex = new MenuItem("索引管理");
+        toolMenu.getItems().add(toolMenuImport);
+        manageMenu.getItems().addAll(manageMenuIndex);
+        toolMenuImport.setOnAction(evnet -> {
+            Parent root = null;
+            try {
+                root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        });
+        manageMenuIndex.setOnAction(event -> {
+            Parent root = null;
+            try {
+                root = FXMLLoader.load(getClass().getResource("IndexManage.fxml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        });
+    }
+}
